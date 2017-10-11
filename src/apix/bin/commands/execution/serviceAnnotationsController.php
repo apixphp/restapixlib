86a078646afdd0ffcd7e32fb9b6c355940b279ef
aligurbuz_beyn <ali.gurbuz@beyn.com.tr>
2017-10-11 07:53:37 +0000

    apix structured

--- src/apix/bin/commands/execution/serviceAnnotationsController.php
+++ src/apix/bin/commands/execution/serviceAnnotationsController.php
@@ -8,5 +8,49 @@ namespace src\app\__projectName__\v1;
 
 trait serviceAnnotationsController
 {
+    /**
+     * @var $redis \src\store\services\redis
+     */
+    public $redis;
+
+    /**
+     * @return \src\store\services\redis
+     */
+    public function redis(){
+
+        $this->redis=app('redis');
+        return $this->redis;
+    }
+
+    /**
+     * @var $collection \src\store\services\appCollection
+     */
+    public $collection;
+
+    /**
+     * @return \src\store\services\appCollection
+     */
+    public function collection(){
+
+        $this->collection=app('collection');
+        return $this->collection;
+    }
+
+    /**
+     * @var $logger \src\app\__projectName__\v1\serviceLogController
+     */
+    public $logger;
+
+    /**
+     * @param $logType string
+     * @param $logFile string
+     * @return \src\app\__projectName__\v1\serviceLogController
+     */
+    public function log($logType,$logFile){
+
+        $this->logger=new \Log($logType,$logFile);
+        return $this->logger;
+    }
+
     //annotationController
 }
