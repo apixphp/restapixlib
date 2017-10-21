295a599747a43831eaa0ee8bffe913c227282fa6
aligurbuz <galiant781@gmail.com>
2017-10-21 14:00:27 +0000

    apix structured

--- src/apix/bin/commands/execution/auth.php
+++ src/apix/bin/commands/execution/auth.php
@@ -1,13 +1,34 @@
 <?php
-/**
- * Apix makes implementing authentication very simple. In fact, almost everything is configured for you out of the box.
- * The authentication configuration file is located at config/auth.php,
- * which contains several well documented options for tweaking the behavior of the authentication services.
- */
-
 namespace Src\App\__projectName__\V1\Config;
 
-class Auth {
+use Src\App\__projectName__\V1\ServiceAnnotationsController;
+use Src\App\__projectName__\V1\ServiceBaseController as Base;
+use Src\App\__projectName__\V1\Model\Sudb\User;
+
+class Auth extends Base {
+
+    //service annotations controller
+    use ServiceAnnotationsController;
+
+    /**
+     * @var mixed
+     */
+    public $input;
+
+    /**
+     * Auth constructor.
+     */
+    public function __construct(){
+
+        parent::__construct();
+        $this->input=$this->request->input();
+
+        if(!isset($this->input['username']) && !isset($this->input['password'])) {
+
+            $this->input['username']='username';
+            $this->input['password']='password';
+        }
+    }
 
     /**
      * @return array
@@ -21,10 +42,10 @@ class Auth {
                 //default user guard
                 'default'=>[
                     'driver'=>'database',
-                    'model'=>\Src\App\__projectName__\V1\Model\Sudb\User::class,
+                    'model'=>User::class,
                     'credentials'=>[
-                        'username'=>'usertest',
-                        'password'=>'123456'
+                        'username'=>$this->input['username'],
+                        'password'=>$this->input['password']
                     ],
                     'registerMethod'=>'session',
                     'tokenField'=>'app_token',
@@ -36,8 +57,11 @@ class Auth {
                 //admin user guard
                 'admin'=>[
                     'driver'=>'database',
-                    'model'=>\Src\App\__projectName__\V1\Model\Sudb\User::class,
-                    'credentials'=>[],
+                    'model'=>User::class,
+                    'credentials'=>[
+                        'username'=>$this->input['username'],
+                        'password'=>$this->input['password']
+                    ],
                     'registerMethod'=>'session',
                     'tokenField'=>'app_token',
                     'encrypt'=>'math',
